49bd0f090a80bc268e251bd183d74029d9d61da3
otabek20041017 <otabek.yusupov20041017@gmail.com>
2022-11-15 21:59:42 +0500

    SQL 14.11.2022-15.11.2022

--- src/test/resources/Quanties/day1/day2/groupBy.sql
+++ src/test/resources/Quanties/day1/day2/groupBy.sql
@@ -21,5 +21,44 @@ select count(distinct job_id) from EMPLOYEES;
  */
 select * from EMPLOYEES;
 
-select JOB_ID,avg(salary), min(salary), max(SALARY), sum(salary) from EMPLOYEES
+
+
+select JOB_ID,avg(salary), min(salary), max(SALARY), sum(salary),count(*) from EMPLOYEES
 group by JOB_ID;
+
+
+-- get me total salary for EACH department from Employees table
+select DEPARTMENT_ID,sum(salary),count(*),max(salary),min(salary),round(avg(salary)) from EMPLOYEES
+where DEPARTMENT_ID is not null     -- to remove null department id from result
+group by DEPARTMENT_ID;
+
+-- order results based on max salary in asc
+select DEPARTMENT_ID,sum(salary),count(*),max(salary),min(salary),round(avg(salary)) from EMPLOYEES
+where DEPARTMENT_ID is not null     -- to remove null department id from result
+group by DEPARTMENT_ID
+order by max(salary);
+
+-- order results based on max salary in asc and min salary desc
+select DEPARTMENT_ID,sum(salary),count(*),max(salary),min(salary),round(avg(salary)) from EMPLOYEES
+where DEPARTMENT_ID is not null     -- to remove null department id from result
+group by DEPARTMENT_ID
+order by max(salary), min(salary) desc;
+
+-- display how many departments we have in each location
+    select LOCATION_ID, count(*) from DEPARTMENTS
+    group by LOCATION_ID;
+
+    -- order results based on number od departments in desc
+    select LOCATION_ID,count(*) from DEPARTMENTS
+    group by LOCATION_ID
+    order by count(*) desc;
+
+    select LOCATION_ID,count(*) from DEPARTMENTS
+    group by LOCATION_ID
+    order by count(*) desc;
+
+-- display how many countries we have in each regions
+select REGION_ID from COUNTRIES
+group by REGION_ID
+order by count(*) desc;
+
